a1ef040ce70c5be34c0584c87f3916fc2973017e
shun7603 <summersao7603@icloud.com>
2025-06-03 00:55:35 +0900

    カウント
    ＋-を押すとカウントが実際に表示されるようにした

--- game.py
+++ game.py
@@ -10,7 +10,13 @@ class App:
     def update(self):
        if pyxel.btnp(pyxel.KEY_ESCAPE):
            pyxel.quit()
-
+           
+       if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
+          if 20 <= pyxel.mouse_x <= 40 and 50 <= pyxel.mouse_y <= 70:
+                self.number -= 1
+          elif 100 <= pyxel.mouse_x <= 120 and 50 <= pyxel.mouse_y <= 70:
+                 self.number += 1
+           
     def draw(self):
         pyxel.cls(pyxel.COLOR_DARK_BLUE)
         pyxel.text(70, 60, f"{self.number}", pyxel.COLOR_YELLOW)
