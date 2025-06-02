20caaf5460fcdd907044b28e52867b6b3ec65c31
shun7603 <summersao7603@icloud.com>
2025-06-03 00:13:29 +0900

    Initial commit

---
+++ game.py
@@ -0,0 +1,15 @@
+import pyxel
+
+class App:
+    def __init__(self):
+        pyxel.init(160, 120, title="サプーゲーム")
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+       if pyxel.btnp(pyxel.KEY_ESCAPE):
+           pyxel.quit()
+
+    def draw(self):
+        pyxel.cls(pyxel.COLOR_DARK_BLUE)
+        pyxel.text(70, 60, "start", pyxel.COLOR_YELLOW)
+App()
