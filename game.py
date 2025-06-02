95b7aa2b05045258428032a4e85f05067efa6ec4
shun7603 <summersao7603@icloud.com>
2025-06-03 00:37:15 +0900

    カウント
    マウスの＋,-をクリックするとカウントされるようにした

--- game.py
+++ game.py
@@ -3,6 +3,8 @@ import pyxel
 class App:
     def __init__(self):
         pyxel.init(160, 120, title="サプーゲーム")
+        pyxel.mouse(True)
+        self.number = 0
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -11,6 +13,8 @@ class App:
 
     def draw(self):
         pyxel.cls(pyxel.COLOR_DARK_BLUE)
-        pyxel.text(70, 60, "start", pyxel.COLOR_YELLOW)
+        pyxel.text(70, 60, f"{self.number}", pyxel.COLOR_YELLOW)
+        pyxel.text(30, 60, "-", pyxel.COLOR_WHITE)
+        pyxel.text(110, 60, "+", pyxel.COLOR_WHITE)
 
 App()
