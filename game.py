cfa0d3125d8839f30c6d28633ff2420a1f618f9d
shun7603 <summersao7603@icloud.com>
2025-06-03 12:05:40 +0900

    プレーヤーを動かす
    self.player変数を用いてプレーヤーの動きを表すようにした。
    左右クリックによってプレーヤーの座標が変更できるようにした。
    尚且つプレーヤーの絵のサイズに合わせて（右12,左4)壁に当たると進まないように設定した。

--- game.py
+++ game.py
@@ -9,12 +9,17 @@ class App:
         pyxel.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="サプーゲーム")
         pyxel.mouse(True)
         pyxel.load("my_resource.pyxres")
+        self.player_x = SCREEN_WIDTH // 2
         pyxel.run(self.update, self.draw)
 
     def update(self):
        if pyxel.btnp(pyxel.KEY_ESCAPE):
            pyxel.quit()
 
+       if pyxel.btn(pyxel.KEY_RIGHT) and self.player_x < SCREEN_WIDTH - 12:
+           self.player_x += 1
+       elif pyxel.btn(pyxel.KEY_LEFT) and self.player_x > -4:
+           self.player_x -= 1
 
            
     def draw(self):
@@ -22,6 +27,7 @@ class App:
         # 石
         pyxel.blt(SCREEN_WIDTH // 2, 0, 0 ,8, 0, 8, 8, pyxel.COLOR_BLACK)
         # プレーヤー
-        pyxel.blt(SCREEN_WIDTH // 2, SCREEN_HEIGHT * 4 // 5, 0, 16, 0, 16, 16, pyxel.COLOR_BLACK)
+        pyxel.blt(self.player_x, SCREEN_HEIGHT * 4 // 5, 0, 16, 0, 16, 16, 
+                  pyxel.COLOR_BLACK)
 
 App()
